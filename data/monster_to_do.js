65b7bb58840716086b27fd49f562fa4dbc500b74
SarahRector <rector.sarah.r@gmail.com>
2020-08-18 16:38:04 -0700

    added routes and endpoints

---
+++ data/monster_to_do.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'terrorize peasants',
+    completed: false,
+  },
+  {
+    todo: 'clean up black lagoon',
+    completed: false,
+  },
+  {
+    todo: 'shopping trip to blood bank',
+    completed: false,
+  }
+];
